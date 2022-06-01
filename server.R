1179515b93e58dc7b7d8f848c39eddce6542dfc3
lxzhe718 <lxzhe718@uw.edu>
2022-06-01 16:18:23 +0800

    finish one chart

--- server.R
+++ server.R
@@ -2,68 +2,62 @@ library(ggplot2)
 library(plotly)
 library(dplyr)
 
-climate_df <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv", stringsAsFactors = FALSE)
+heart_df <- read.csv("heart.csv", stringsAsFactors = FALSE)
+heart_df <- na.omit(heart_df)
+heart_df <- heart_df %>% filter
 
-# use for summ tab
-china_data <- climate_df %>% filter(country == "China")
-# since Chinese data only from 1899, so make them consistence.
-american_data <- climate_df %>% filter(country == "United States", year >= 1899)
+have_disease <- heart_df %>% filter(HeartDisease == "Yes")
+no_disease <- heart_df %>% filter(HeartDisease == "No")
+
+# get number of rows in order to calculate percentage.
+total_have_disease = nrow(have_disease)
+total_no_disease = nrow(no_disease)
 
 server <- function(input, output) {
-  output$co2_plot <- renderPlotly({
-    filtered_df <- climate_df %>%
-      filter(country %in% input$country_selection) %>%
-      filter(year >= input$year_selection[1] & 
-               year <= input$year_selection[2])
-
-    plot1 <- ggplot(data = filtered_df) +
-      geom_line(mapping = aes(x = year, y = co2, color = country)) + 
-      labs(title = "Co2 vs. Year", y = "Co2 in million tonnes")
+  output$factor_plot <- renderPlotly({
+    # For people have heart disease, see the count of having given factor or not
+    factor_have_disease <- have_disease %>% 
+      group_by_at(input$factor_selection) %>%
+      summarise(count_have_disease = n())
+    
+    # For people do not have heart disease, see the count of having given factor or not
+    factor_no_disease <- no_disease %>% 
+      group_by_at(input$factor_selection) %>% 
+      summarise(count_no_disease = n())
+    
+    # Join two data together and calculate the percentage
+    all_df <- factor_have_disease %>% 
+      left_join(factor_no_disease, by=input$factor_selection) %>%
+      mutate(all_count = count_have_disease + count_no_disease,
+             have_disease_percentage = 
+               count_have_disease / all_count * 100,
+             no_disease_percentage =  
+               count_no_disease / all_count * 100)
+    
+    selected_df <- all_df %>% 
+      select(input$factor_selection,
+             "have_disease_percentage",
+             "no_disease_percentage")
+    
+    pivot_df <- selected_df %>% 
+      pivot_longer(!c(input$factor_selection), 
+                   names_to = "category",
+                   values_to = "percentage")
+    
+    # since user select value is string and can't pass into ggplot, so I change corresponding column so I can have control over. 
+    colnames(pivot_df)[which(names(pivot_df) == input$factor_selection)] <- "factor_condition"
+    
+    plot1 <- ggplot(pivot_df, aes(fill=category, 
+                         x=factor_condition,
+                         y=percentage)) +
+      geom_bar(position="dodge", stat="identity") +
+      labs(title = paste("Relationship between", 
+                         input$factor_selection,
+                         "And Heart Disease"), 
+           x = input$factor_selection, 
+           y = "Percentage")
     return(plot1)
   })
   
-  output$type_emit_plot <- renderPlotly({
-    filtered_df <- climate_df %>%
-      filter(country ==  "United States" | country ==  "China")
-    plot_title <- paste("Co2 emit from", gsub("_.*", "", input$type_selection)) 
-                        
-    # since user select value is string and can't pass into ggplot, so I change corresponding column so I can have control over. 
-    colnames(filtered_df)[which(names(filtered_df) == input$type_selection)] <- "new_col"
-    plot2 <- ggplot(data = filtered_df) +
-      geom_line(mapping = aes(x = year, y = new_col, color = country)) + 
-      labs(title = plot_title, y = "Co2 in million tonnes")
-    return(plot2)
-  })
-  
-
-  output$table_china <- renderTable({
-    # get change of co2 emit for each year
-    china_data <- china_data %>% 
-      arrange(year) %>% mutate(change = co2 - lag(co2))
-    top5_change_china <- china_data %>%
-      arrange(desc(change)) %>% slice(1:5) %>% select(year, co2, change)
-    
-    american_data <- american_data %>%
-      arrange(year) %>% mutate(change = co2 - lag(co2))
-    top5_change_us <- american_data %>%
-      arrange(desc(change)) %>% slice(1:5) %>% select(year, co2, change)
-    return(top5_change_china)
-  })
-  
-  output$table_us <- renderTable({
-    # get change of co2 emit for each year
-    american_data <- american_data %>%
-      arrange(year) %>% mutate(change = co2 - lag(co2))
-    top5_change_us <- american_data %>%
-      arrange(desc(change)) %>% slice(1:5) %>% select(year, co2, change)
-    return(top5_change_us)
-  })
-
-  output$avg_co2 <- renderText({
-    avg_ch <- china_data %>% summarise(avg = mean(co2, na.rm = TRUE)) %>% pull(avg)
-    avg_us <- american_data %>% summarise(avg = mean(co2, na.rm = TRUE)) %>% pull(avg)
-    msg <- paste("Chinese average co2 emit from 1899 is", avg_ch, "U.S average co2 emit from 1899 is", avg_us)
-    return(msg)
-  })
   
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,6 @@
 library(plotly)
 
-# Load climate data
-climate_df <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv", stringsAsFactors = FALSE)
+heart_df <- read.csv("heart.csv", stringsAsFactors = FALSE)
 
 # Home page tab
 intro_tab <- tabPanel(
@@ -12,78 +11,75 @@ intro_tab <- tabPanel(
   )
 )
 
-summ_tab <- tabPanel(
-  # Title of tab
-  "Summary of the data",
-  fluidPage(
-    p("Chinese data"),
-    tableOutput(outputId = "table_china"),
-    p("US data"),
-    tableOutput(outputId = "table_us"),
-    textOutput(outputId = "avg_co2")
-  )
-)
-
 # Create sidebar panel for widget
-sidebar_panel_widget <- sidebarPanel(
-  checkboxGroupInput(
-    inputId = "country_selection",
-    label = "Choice the filter country",
-    choices = c("United States", "China"),
-    selected = "China"
-    ),
-  sliderInput(
-    inputId = "year_selection",
-    label = h3("Choose Year range"),
-    min = min(climate_df$year),
-    max = max(climate_df$year),
-    sep = "",
-    value = c(2010, 2020)
-  )
-)
-
-co2_year_plot <- mainPanel(
-  plotlyOutput(outputId = "co2_plot")
-)
-
-co2_tab <- tabPanel(
-  "Co2 Viz",
-  sidebarLayout(
-    sidebar_panel_widget,
-    co2_year_plot
-  ),
-  p("This page shows how co2 emit change over year. The graph will give us direct comparison on co2 emit between China and U.S. From the graph, we can see U.S. have higher co2 emit until 2005 and their is a period in China that have huge increase of co2 emit from 2000")
-)
-
-type_emit_widget <- sidebarPanel(
+first_widget <- sidebarPanel(
   selectInput(
-    inputId = "type_selection",
-    label = h3("Choose source of Co2"),
-    choices = c("Co2 from coal" = "coal_co2",
-                "Co2 from flaring" =  "flaring_co2", 
-                "Co2 from gas" =  "gas_co2",
-                "Co2 from oil" = "oil_co2")
+    inputId = "factor_selection",
+    label = "Choice the factor to see if it has correlation with heart desease",
+    choices = c("Smoking", "Drinknig alcohol" = "AlcoholDrinking",
+                "Stroke", "Diabetic", "Asthma",
+                "Kidney disease" = "KidneyDisease"),
+    selected = "Smoking"
+    )
+)
+
+first_plot <- mainPanel(
+  plotlyOutput(outputId = "factor_plot")
+)
+
+first_tab <- tabPanel(
+  "Heart disease's factors",
+  sidebarLayout(
+    first_widget,
+    first_plot
+  ),
+  p("Detecting and preventing the factors that have the greatest impact on heart disease is very important in healthcare. So this page shows if a factor have correlation wirh heart disease. By choosing given factor, the graph will show the percentage of heart disease and whether have the factor or not.")
+)
+
+second_widget <- sidebarPanel(
+  selectInput(
+    inputId = "factor_selection",
+    label = "Choice the factor to see if it has correlation with heart desease",
+    choices = c("Smoking", "Drinknig alcohol" = "AlcoholDrinking",
+                "Stroke", "Diabetic", "Asthma",
+                "Kidney disease" = "KidneyDisease"),
+    selected = "Smoking"
   )
 )
 
-type_plot <- mainPanel(
-  plotlyOutput(outputId = "type_emit_plot")
+second_plot <- mainPanel(
+  plotlyOutput(outputId = "factor_plot")
+)
+second_tab <- tabPanel(
+  "Heart disease's factors",
+  sidebarLayout(
+    second_widget,
+    second_plot
+  ),
+  p("add decription here")
 )
 
-emit_tab <- tabPanel(
-  "Source of emit",
+
+third_widget <- sidebarPanel(
+
+)
+
+third_plot <- mainPanel(
+  plotlyOutput(outputId = "factor_plot")
+)
+third_tab <- tabPanel(
+  "Heart disease's factors",
   sidebarLayout(
-    type_emit_widget,
-    type_plot
+    third_widget,
+    third_plot
   ),
-  p("This plot shows different source of co2, we can see in China, cole contribute huge amount of co2. In other aspect, U.S have higher amount.")
+  p("add decription here.")
 )
 
 ui <- navbarPage(
   # Home page title
   "A5: Co2 emission",
   intro_tab,
-  co2_tab,
-  emit_tab,
-  summ_tab
+  second_tab,
+  third_tab
 )
