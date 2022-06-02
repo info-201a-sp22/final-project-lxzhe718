f27f3e3226ef4736ca3e62fcd808366763155b46
heyao-1936277 <81781912+heyao-1936277@users.noreply.github.com>
2022-06-02 06:57:49 -0700

    Completed

--- ui.R
+++ ui.R
@@ -45,7 +45,7 @@ first_tab <- tabPanel(
     first_widget,
     first_plot
   ),
-  p("Detecting and preventing the factors that have the greatest impact on heart disease is very important in healthcare. So this page shows if a factor have correlation wirh heart disease. By choosing given factor, the graph will show the percentage of heart disease and whether have the factor or not.")
+  p("the graph is conducted to explore the correlation bewteen people with or without heart disease and their heart disease indicators. The population is splited into two categories pf people(heart disease and no heart diesease) The users are able to choose which factor they want to study specific. By choosing given factor, the graph will show the percentage of heart disease and compare the two groups of people.")
 )
 
 second_widget <- sidebarPanel(
@@ -75,7 +75,7 @@ second_tab <- tabPanel(
     second_widget,
     second_plot
   ),
-  p("This visualization shows how age and gender relate to heart disease. From the graph, we can see that as the age increase, the percentage of people who getting heart disease in increasing. So their is some relationship between age and heart disease. Interestingly, the rate of heart disease increase more among men than woman and men have higher rate of getteing heart disease.")
+  p("This visualization shows how age and gender relate to heart disease. From the graph, we can see that as the age increases, the percentage of people who getting heart disease is increasing. Interestingly, the rate of heart disease increase more among men than woman and men have higher rate of getting heart disease.")
 )
 
 
@@ -100,6 +100,16 @@ third_tab <- tabPanel(
   p("The main goal of this chart is to explore the relationship between average sleep hours and the chances of getting heart disease. The percentages presented above were calculated by using the number of people who got heart disease to devide by total number of people in each sleep hour category. Regardless of the abnormal data (more tha 16 hours or less than 2 hours), the graph shows that there are slightly more chances to get heart disease if people sleep more than 9 hours or less than 5 hours.If the abnormal datas are included, the graph shows an unstable trend with the factor of sleep hours. One hypothesis of the phenomenon is that, for people who sleeps more than 20, heart disease might be the causes of drowsiness instead of the result.")
 )
 
+# Conclusion/Summary page
+
+con_tab <- tabPanel( 
+  "Conclusion/Summary",
+  fluidPage(
+    includeMarkdown("conclusion.md")
+  )
+)
+
+
 ui <- navbarPage(
   # Home page title
   "Final Project",
@@ -107,6 +117,7 @@ ui <- navbarPage(
   intro_tab,
   first_tab,
   second_tab,
-  third_tab
+  third_tab,
+  con_tab
 )
 
